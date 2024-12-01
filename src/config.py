a29902f18ad835b1e4f426d0eb58e11cd726f71b
PhatCM <phat.caominh.dev@gmail.com>
2024-12-01 20:37:27 +0700

    update config.py

--- src/config.py
+++ src/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     JWT_ALGORITHM: str
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6739
+    REDIS_URL: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
