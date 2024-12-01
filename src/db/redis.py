e2defe8658cce660330caf3b38a7e77ecbb1fd27
PhatCM <phat.caominh.dev@gmail.com>
2024-12-01 20:37:02 +0700

    update redis setting

--- src/db/redis.py
+++ src/db/redis.py
@@ -3,14 +3,16 @@ from src.config import Config
 
 JTI_EXPIRY = 3600
 
-token_blacklist = redis.Redis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=0)
+# URL-based connection
+token_blacklist = redis.from_url(Config.REDIS_URL, decode_responses=True)
 
 
+# Add JTI to blacklist with expiration time
 async def add_jti_to_blacklist(jti: str) -> None:
     await token_blacklist.set(name=jti, value="", ex=JTI_EXPIRY)
 
 
+# Check if JTI is in blacklist
 async def token_in_blacklist(jti: str) -> bool:
-    jti = await token_blacklist.get(jti)
-
-    return jti is not None
+    jti_value = await token_blacklist.get(jti)
+    return jti_value is not None
